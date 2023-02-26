9c23e19264ec789077f638c28cb12de4e56a69e7
shutyoureyestosee <85564422+shutyoureyestosee@users.noreply.github.com>
2023-02-26 12:05:12 +0530

    Update prog.js

--- prog.js
+++ prog.js
@@ -1,47 +1,41 @@
 const http = require('http');
-const pets = [
-  {
-    name: "Meowsy",
-    type: "cat",
-    likes: ["tuna", "catnip"],
-    dislikes: ["ham", "zucchini"]
-  },
-  {
-    name: "Barky",
-    type: "dog",
-    likes: ["bones", "carrots"],
-    dislikes: ["tuna"]
-  },
-  {
-    name: "Purrpaws",
-    type: "cat",
-    likes: ["mice"],
-    dislikes: ["cookies"]
-  }
-];
+const fs = require('fs');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('<!DOCTYPE html>');
-  res.write('<html>');
-  res.write('<head>');
-  res.write('<title>JSON and AJAX</title>');
-  res.write('</head>');
-  res.write('<body>');
-  res.write('<h1>JSON and AJAX</h1>');
-  res.write('<button onclick="fetchContent()">Fetch the content</button>');
-  res.write('<div id="content"></div>');
-  res.write('<script>');
-  res.write('function fetchContent() {');
-  res.write('const pets = ' + JSON.stringify(pets) + ';');
-  res.write('let content = "";');
-  res.write('pets.forEach(pet => {');
-  res.write('content += `${pet.name} is a ${pet.type} that likes to eat ${pet.likes.join(" and ")} and dislikes ${pet.dislikes.join(" and ")}.<br>`;');
-  res.write('});');
-  res.write('document.getElementById("content").innerHTML = content;');
-  res.write('}');
-  res.write('</script>');
-  res.write('</body>');
-  res.write('</html>');
-  return res.end();
+  if (req.url === '/pets') {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    const pets = [
+      {
+        name: "Meowsy",
+        type: "cat",
+        likes: ["tuna", "catnip"],
+        dislikes: ["ham", "zucchini"]
+      },
+      {
+        name: "Barky",
+        type: "dog",
+        likes: ["bones", "carrots"],
+        dislikes: ["tuna"]
+      },
+      {
+        name: "Purrpaws",
+        type: "cat",
+        likes: ["mice"],
+        dislikes: ["cookies"]
+      }
+    ];
+    res.end(JSON.stringify(pets));
+  } else {
+    fs.readFile('index.html', function(err, data) {
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        return res.end("404 Not Found");
+      } 
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
+  }
 }).listen(8080);
+
+
